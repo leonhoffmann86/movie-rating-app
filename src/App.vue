fcc96b94bcc045843c811031c83970b0b7d62dcd
Leon Hoffmann <leon.hoffmann@beawwwer.de>
2020-04-16 13:26:51 +0200

    first commit, setting up vue, veutify and template incl routing

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <router-link v-bind:to="{ name: 'Home' }" class="side_bar_link">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link v-bind:to="{ name: 'Contact' }" class="side_bar_link">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>fas fa-search</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Contact</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-container fluid>
+            <div id="app">
+              <router-view />
+            </div>
+          </v-container>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
